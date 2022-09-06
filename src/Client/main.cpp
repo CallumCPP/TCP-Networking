ab2b35954c655cf86f6a9864540fee5742dc8bc2
GamePP <callumtomkins260505@gmail.com>
2022-09-06 14:26:02 +0100

    update to include asio

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <asio.hpp>
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
---
+++ src/Server/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <asio.hpp>
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
